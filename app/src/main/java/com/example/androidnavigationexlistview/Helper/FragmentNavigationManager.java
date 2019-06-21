96932651f90d724055349531203ca91dc71c4b57
vaibgups <vaibgups@gmail.com>
2019-06-21 17:05:46 +0530

    Initial commit

---
+++ app/src/main/java/com/example/androidnavigationexlistview/Helper/FragmentNavigationManager.java
@@ -0,0 +1,33 @@
+package com.example.androidnavigationexlistview.Helper;
+
+import android.support.v4.app.FragmentManager;
+
+import com.example.androidnavigationexlistview.Interface.NavigationManager;
+import com.example.androidnavigationexlistview.MainActivity;
+
+public class FragmentNavigationManager implements NavigationManager {
+
+    private static FragmentNavigationManager mInstance;
+
+    private FragmentManager mFragmentManager;
+    private MainActivity mainActivity;
+
+    public static FragmentNavigationManager getInstance(MainActivity mainActivity){
+        if (mInstance == null)
+            mInstance = new FragmentNavigationManager();
+        mInstance.configure(mainActivity);
+        return mInstance;
+    }
+
+    private void configure(MainActivity mainActivity) {
+
+        this.mainActivity = mainActivity;
+        mFragmentManager = mainActivity.getSupportFragmentManager();
+
+    }
+
+    @Override
+    public void showFragment(String title) {
+
+    }
+}
