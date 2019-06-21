3a0c4bd1e7a7f6a7f7f2bdae7d11bcc7cd5ee2f1
vaibgups <vaibgups@gmail.com>
2019-06-21 17:58:42 +0530

    Inflate menu in main activity

--- app/src/main/java/com/example/androidnavigationexlistview/MainActivity.java
+++ app/src/main/java/com/example/androidnavigationexlistview/MainActivity.java
@@ -1,11 +1,31 @@
 package com.example.androidnavigationexlistview;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ExpandableListAdapter;
+import android.widget.ExpandableListView;
+
+import com.example.androidnavigationexlistview.Interface.NavigationManager;
+
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+    private String mActivityTitle;
+    private String [] items;
+
+    private ExpandableListView expandableListView;
+    private ExpandableListAdapter expandableListAdapter;
+    private List<String> lstTile;
+    private Map<String,List<String>> lstChild;
+    private NavigationManager navigationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
